9b8b654249e314a1ce3b6912df9c7e06f381d855
okwunka <okwunkaifeanyi@gmail.com>
2023-02-24 00:36:40 -0800

    Deleting

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -5,13 +5,14 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	print_number(98);
+	print_numbers(98);
 	_putchar('\n');
-	print_number(402);
+	print_numbers(402);
 	_putchar('\n');
-	print_number(1024);
+	print_numbers(1024);
 	_putchar('\n');
 	print_number(0);
 	_putchar('\n');
